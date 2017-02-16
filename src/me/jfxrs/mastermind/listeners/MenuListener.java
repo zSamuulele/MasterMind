97be9ac11f4ea7d9703fda67528831a17676ed28
iAmGio <gio@gio.it>
2017-02-16 15:53:15 +0100

    Added a little signature

--- src/me/jfxrs/mastermind/listeners/MenuListener.java
+++ src/me/jfxrs/mastermind/listeners/MenuListener.java
@@ -18,7 +18,12 @@ public class MenuListener implements Listener {
     public void onMenuClick(MenuButtonClickEvent e) {
         if(MasterMind.getGame() == null) {
             Animation animation = new Animation(Animation.Type.MOVEMENT_Y, -1000, Duration.seconds(0.3), false);
-            JavaFX.getRoot().getChildrenUnmodifiable().forEach(animation::play);
+            JavaFX.getRoot().getChildrenUnmodifiable().forEach(n -> {
+                if(!(n.getId() != null && n.getId().equals("mastermind"))) {
+                    animation.play(n);
+                }
+                else new Animation(Animation.Type.OPACITY, 0, Duration.seconds(0.2), false).play(JavaFX.fromId("mastermind"));
+            });
             animation.setOnAnimationCompleted(() -> MasterMind.setGame(new Game(e.getGameMode(), true)));
         }
     }
